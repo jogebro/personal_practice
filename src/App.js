cb6e03dcd34b89cb561087a40bcc5b1e42bf7e06
jogebro <joelabril2110@gmail.com>
2023-09-19 12:18:06 -0500

    feat: :sparkles: practica personal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTask] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const aggnewTask = ()=>{
+    if (newTask.trim() !== '') {
+      setTask([...tasks, newTask]);
+      setNewTask('');
+    }
+  }
+
+  const deleteTask = (index)=>{
+    const tarea = [...tasks];
+    tarea.splice(index, 1);
+    setTask(tarea);
+  }
+
+  const editTask = (index)=>{
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Lista de Videojuegos</h1>
+          <input 
+          type='text'
+          placeholder='Agregar Juego'
+          value={newTask}
+          onChange={(e)=> setNewTask(e.target.value)}
+          />
+          <button onClick={aggnewTask}>Guardar</button>
+        </div>
+        <div>
+          <ul>
+            {tasks.map((task, index)=>(
+              <li key={index}>
+                {task}
+                <button onClick={() => deleteTask(index)}>borrar</button>
+                <button onClick={() => editTask(index)}>editar</button>
+              </li>
+            ))
+            }
+          </ul>
+        </div>
       </header>
     </div>
   );
